9416b898b35e96e9a26fd90004bb4757c77eb2af
yu@sec.com <yu@sec.com>
2017-08-03 16:37:00 +0800

    可以自动生成模板了.

--- src/background.js
+++ src/background.js
@@ -7,12 +7,3 @@ const tab_log = function(json_args) {
     var args = JSON.parse(unescape(json_args));
     console[args[0]].apply(console, Array.prototype.slice.call(args, 1));
 }
-
-chrome.devtools.network.onRequestFinished.addListener(
-    function(request) {
-        chrome.devtools.inspectedWindow.eval(
-            'console.log("Large image: " + unescape("' +
-            escape(request.request.url) + '"))');
-        console.log("---")
-    }
-);
--- src/devtool.js
+++ src/devtool.js
@@ -1,10 +1,60 @@
 
-
 chrome.devtools.network.onRequestFinished.addListener(
     function(request) {
-             chrome.devtools.inspectedWindow.eval(
-                'console.log("Large image: " + unescape("' +
-                escape(request.request.url) + '"))');
-             console.log("---")
+
+        try {
+            if (request.request.url.toString().indexOf("/api/") > -1) {
+                 chrome.devtools.inspectedWindow.eval('console.log(' +JSON.stringify(request) + ')');
+                // //注意这是回调方法
+                //
+                // request.getContent((response)=>{
+                //     chrome.devtools.inspectedWindow.eval('console.log(' +JSON.stringify(response) + ')');
+                // });
+                // //注意这是回调方法
+                // chrome.devtools.network.getHAR((log)=>{
+                //     chrome.devtools.inspectedWindow.eval('console.log(' +JSON.stringify(log) + ')');
+                // })
+
+                var tpl="";
+                tpl+="| script | Connect Server| "+spUrl(request.request)+"|\\n"
+                tpl+=fillBodyOrParam(request.request)
+                tpl+="| "+request.request.method.toLowerCase()+"|\\n"
+                tpl+="| check  | json Structure| code,msg,body | true  |\\n"
+                tpl+="| check  | json Value    | msg            | OK    |\\n"
+
+                chrome.devtools.inspectedWindow.eval('console.log("' +tpl + '")');
+            }
+        } catch (e) {
+            chrome.devtools.inspectedWindow.eval('console.log("error")');
+
+        }
     }
+
 );
+chrome.devtools.inspectedWindow.eval('console.log(' +JSON.stringify(chrome.devtools)+ ')');
+
+/**
+ * 分离URL,导致参数重复
+ */
+function spUrl(req) {
+    if(req.method=="GET"){
+        return req.url.split("?")[0]
+    }
+    return req.url
+}
+/**
+ * 根据GET或POST生成模板
+ * @param req
+ * @return {*}
+ */
+function fillBodyOrParam(req){
+    if(req.method=="POST"){
+        return "| set Body|"+req.postData.text+"|\\n"
+    }else if(req.method=="GET"){
+        var out=""
+        for(var item of req.queryString){
+            out+="| set Param | " +item.name +" |   | "+item.value+"  |\\n"
+        }
+        return out
+    }
+}
