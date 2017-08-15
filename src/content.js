da807e3a2cedc4aef6089925e6245a15ae4d1676
yu@sec.com <yu@sec.com>
2017-08-15 15:25:01 +0800

    添加对话框

--- src/content.js
+++ src/content.js
@@ -2,10 +2,29 @@
  * Created by yu on 2017/7/21.
  */
 
-console.log(document.getElementsByClassName("path-li"))
+//console.log(document.getElementsByClassName("path-li"))
 
 
 var HAS_UPDATED=false
+var base_ip="http://172.16.52.181:9090/"
+var sc=`<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>`
+var jq=`<script src="http://code.jquery.com/jquery-1.12.4.min.js"     integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="     crossorigin="anonymous"></script>`
+var jqtheme=`<link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>`
+var mo=`<form>
+             <label for="name">FitNesse Server IP</label><br/>
+            <input type="text" name="ip" id="ip" value="http://172.16.52.181:9090/" class="text ui-widget-content ui-corner-all" style="width: 400px;"><br/>
+            <label for="email">Path</label><span id="helpText"></span><br/>
+            <input type="text" name="path" id="path" value="LieLuoBo" class="text ui-widget-content ui-corner-all"><br/>
+            <label for="content">Content</label><br/>
+            <textarea  name="content" id="content" class="text ui-widget-content ui-corner-all"   style="width: 500px;  height: 150px;"  >
+!define url (\${base_url}/bi/cw/detail/interviews)
+!contents -R2 -g -p -f -h
+            </textarea>
+            <br/>
+    
+            <!-- Allow form submission with keyboard without duplicating the dialog button -->
+            <input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
+         </form>`
 
 var waiter=setInterval(()=>{
     console.log("running")
@@ -16,10 +35,23 @@ var waiter=setInterval(()=>{
     if(HAS_UPDATED)
         return
 
+
+    $(".project-title").append("<div id='dialog-form'>"+mo+"</div>")
+    $(".project-title").append(jq)
+    $(".project-title").append(sc)
+    $(".project-title").append(jqtheme)
+
+    showFitnesseCreateDialog()
+
     $(".path-li .path-head .middle-right").append("<span class='h-tag _sp-button' >...</span>")
+    $(".path-li .path-head .middle-right").append("<span class='h-tag _fit-button' >+></span>")
+
     $(".path-li .path-head").map((i,dom)=>{
         var method=$(dom).find(".path-method").text()
         var url=$(dom).find(".path-name").text()
+
+        //添加转义文字
+       // $(dom).find(".path-name").after(" <span class='path-description'>"+toUpWord(url)+"</span>")
 
         $(dom).find("._sp-button").text(getTempText(method+":"+url));
 
@@ -27,7 +59,22 @@ var waiter=setInterval(()=>{
             e.stopPropagation();
             toggleTempMark(method+":"+url,$(dom).find("._sp-button"))
         })
+        $(dom).find("._fit-button").on("click",(e)=>{
+            e.stopPropagation();
+            if(localStorage.ip)     $("#ip").val(localStorage.ip)
+            //if(localStorage.path)     $("#path").val(localStorage.path)
+            //if(localStorage.content)     $("#content").val(localStorage.content)
+            $("#content").val(`!define url (\${base_url}${url})\n!contents -R2 -g -p -f -h`)
+            $("#path").val(toUpWord(url))
+            $("#helpText").text(url+"  "+$(dom).find(".path-description").text())
+            FIT_NESSE_DIALOG.dialog("open")
+        })
     })
+
+    $("#ip").on("change",(v)=>localStorage.ip=v.target.value)
+    $("#path").on("change",(v)=>localStorage.path=v.target.value)
+    $("#content").on("change",(v)=>localStorage.content=v.target.value)
+
     var style="    margin-left: 14px;  color: white;  background: #61affe;"
 
 
@@ -39,7 +86,34 @@ var waiter=setInterval(()=>{
     clearInterval(waiter);
 },1000)
 
+var FIT_NESSE_DIALOG
+var showFitnesseCreateDialog=()=>{
+    FIT_NESSE_DIALOG=$("#dialog-form").dialog({
+        autoOpen: false,
+        height: 500,
+        width: 600,
+        modal: true,
+        buttons: {
+            "Create A Suite":fitnesseAddChild,
+            Cancel: function() {
+                FIT_NESSE_DIALOG.dialog( "close" );
+            }
+        },
+        close: function() {
+        }
+    });
+}
 
+var fitnesseAddChild=()=>{
+    $.get($("#ip").val()+"?addChild",{
+            pageName:$("#path").val(),
+            pageType:"Suite",
+            helpText:$("#helpText").text(),
+            pageContent:$("#content").val()
+    },()=>{
+            FIT_NESSE_DIALOG.dialog("close")
+    })
+}
 var exportProgressState=false;
 var exportProgress=()=>{
     var out={}
@@ -135,3 +209,17 @@ var getTempText=(pathName)=>{
         return "OK"
     }
 }
+
+/**
+ *  /bi/api ==>BiApi
+ */
+var toUpWord=(str)=>{
+    var arr=str.split("/")
+    for(var i=0;i<arr.length;i++){
+        var tmp=arr[i]
+         arr[i]= tmp.substring(0,1).toUpperCase()+tmp.substring(1,tmp.length);
+    }
+    return arr.join("")
+}
+
+//console.log(toUpWord("/bi/cw/detail/entering"))
