85779a4b51dfc9dd1f19f30a143580123a52f9b5
yu@sec.com <yu@sec.com>
2018-01-16 16:52:47 +0800

    save ok

--- src/devpanel.js
+++ src/devpanel.js
@@ -3,5 +3,5 @@
 /**
    do nothing here ...
 
-
  */
+
--- src/devtool.js
+++ src/devtool.js
@@ -7,7 +7,11 @@ chrome.devtools.network.onRequestFinished.addListener(
         try {
             if (request.request.url.toString().indexOf("/api/") > -1) {
 
-                chrome.devtools.inspectedWindow.eval('console.log(' +JSON.stringify(request) + ')');
+                //chrome.devtools.inspectedWindow.eval('console.log(' +JSON.stringify(request) + ')');
+                if( isInBlackList(request.request.url)){
+                    return;
+                }
+
                 // //注意这是回调方法
                 //
                 // request.getContent((response)=>{
@@ -25,46 +29,119 @@ chrome.devtools.network.onRequestFinished.addListener(
                 tpl+="| check  | json Structure| code,msg,body | true  |\\n"
                 tpl+="| check  | json Value    | msg            | OK    |\\n"
 
-                chrome.devtools.inspectedWindow.eval('window.FIT_LIST=[];FIT_LIST.push("' +tpl + '")');
-                chrome.devtools.inspectedWindow.eval('console.log(FIT_LIST)');
-                chrome.devtools.inspectedWindow.eval('console.log("' +tpl + '")');
+                //chrome.devtools.inspectedWindow.eval('window.FIT_LIST=[];FIT_LIST.push("' +tpl + '")');
+                //chrome.devtools.inspectedWindow.eval('console.log(FIT_LIST)');
+                //chrome.devtools.inspectedWindow.eval('console.log("' +tpl + '")');
 
-                chrome.extension.sendMessage(JSON.stringify(request.request))
-                chrome.extension.sendMessage("XX");
-                if(__PANEL_DOCUMENT__!=null){
-                    __PANEL_DOCUMENT__.document.querySelector("#api_root").innerHTML = JSON.stringify(request.request);
-                }
 
-                chrome.runtime.sendMessage(chrome.devtools.inspectedWindow.tabId,tpl)
-
+                appendToApiPanel(request.request)
             }
         } catch (e) {
-            chrome.devtools.inspectedWindow.eval('console.log("error")');
-
+            chrome.devtools.inspectedWindow.eval('console.error(' +JSON.stringify(e) + ')');
         }
     }
 
 );
+
+if (!('toJSON' in Error.prototype)){
+    Object.defineProperty(Error.prototype, 'toJSON', {
+        value: function () {
+            var alt = {};
+
+            Object.getOwnPropertyNames(this).forEach(function (key) {
+                alt[key] = this[key];
+            }, this);
+
+            return alt;
+        },
+        configurable: true,
+        writable: true
+    });
+}
+
+function buildFitScript(request) {
+    var tpl="";
+    tpl+="| script | Connect Server| "+spUrl(request)+"|\n"
+    tpl+=fillBodyOrParamNotExt(request)
+    tpl+="| "+request.method.toLowerCase()+"|\n"
+    tpl+="| check  | json Structure| code,msg,body | true  |\n"
+    tpl+="| check  | json Value    | msg            | OK    |\n"
+    return tpl;
+}
+function buildPythonScript(request) {
+    //TODO
+}
 /**
  *  use global function to save info.
  *  代替 devpanel.js来处理dom元素.
  */
+var PANEL_SESSION={
+    document:null,
+    requests:[],
+    currentType:1,
+    filterBlackList:["api/v1/spans"]
+}
 
-var __PANEL_DOCUMENT__=null
 chrome.devtools.panels.create("API",
     null,
     "./src/devpanel.html",
     function(panel) {
         panel.onShown.addListener(function(win) {
-            console.log('i think this is the right onshow');
-            var status = win.document.querySelector("#api_root");
-            status.innerHTML = "Fixing to make magic.";
-            __PANEL_DOCUMENT__=win
+            PANEL_SESSION.document=win.document
+            PANEL_SESSION.document.querySelector('#typeFitnesse').addEventListener('click', function() {
+                PANEL_SESSION.currentType=1
+            }, false);
+            PANEL_SESSION.document.querySelector('#typePython').addEventListener('click', function() {
+                PANEL_SESSION.currentType=2
+            }, false);
+            PANEL_SESSION.document.querySelector('#clearSession').addEventListener('click', function() {
+                clearSession()
+            }, false);
+
+            initSession()
         });
-        // code invoked on panel creation
     }
 );
+function isInBlackList(url) {
+    let out=false
+    PANEL_SESSION.filterBlackList.map((str)=>{
+        if(url.indexOf(str)>-1){
+            out=true
+        }
+    })
+    return out
+}
 
+function clearSession() {
+    PANEL_SESSION.requests=[]
+}
+function initSession() {
+    PANEL_SESSION.requests.map(t=>pureAdd(t))
+}
+function pureAdd(obj) {
+    if(isInBlackList(obj.url)) return
+    if(PANEL_SESSION.document!=null){
+        let intext=null
+        if(PANEL_SESSION.currentType==1){
+            intext=buildFitScript(obj)
+        }else if(PANEL_SESSION.currentType==2){
+            intext=buildPythonScript(obj)
+        }else if(PANEL_SESSION.currentType==0){
+            intext=JSON.stringify(obj)
+        }
+        let root= PANEL_SESSION.document.querySelector("#api_root");
+        let p = PANEL_SESSION.document.createElement("pre");
+        p.innerHTML=intext
+        root.append(p);
+        PANEL_SESSION.document.querySelector("#api_count").innerHTML=PANEL_SESSION.requests.length;
+        PANEL_SESSION.document.querySelector("#api_type").innerHTML=PANEL_SESSION.currentType;
+    }
+}
+
+function appendToApiPanel(obj) {
+    PANEL_SESSION.requests.push(obj)
+    pureAdd(obj)
+}
 
 /**
  * 分离URL,导致参数重复
@@ -82,11 +159,28 @@ function spUrl(req) {
  */
 function fillBodyOrParam(req){
     if(req.method=="POST"){
+        if(req.postData.text!=null &&req.postData.text.length>60 ){
+            return "| set Body| {{{ \\n"+excp(JSON.stringify(JSON.parse(req.postData.text),0,4))+"\\n }}}|\\n"
+        }
         return "| set Body| "+excp(req.postData.text)+" |\\n"
     }else if(req.method=="GET"){
         var out=""
         for(var item of req.queryString){
             out+="| set Param | " +item.name +" |   | "+item.value+"  |\\n"
+        }
+        return out
+    }
+}
+function fillBodyOrParamNotExt(req){
+    if(req.method=="POST"){
+        if(req.postData.text!=null &&req.postData.text.length>60 ){
+            return "| set Body| {{{ \n"+JSON.stringify(JSON.parse(req.postData.text),0,4)+" \n }}}|\n"
+        }
+        return "| set Body| "+req.postData.text+" |\n"
+    }else if(req.method=="GET"){
+        var out=""
+        for(var item of req.queryString){
+            out+="| set Param | " +item.name +" |   | "+item.value+"  |\n"
         }
         return out
     }
